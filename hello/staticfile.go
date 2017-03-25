8bf3fd889d7fe60bfec5872dc8e2ad2360542ab2
laivietanh1986 <laivietanh1986@gmail.com>
2017-03-25 11:32:07 +0700

    change to hacker rank

--- hello/staticfile.go
+++
@@ -1,9 +0,0 @@
-package main
-import (
-    "net/http"
-)
-func main()  {
-    fs:=http.FileServer(http.Dir("templates/"))
-    http.Handle("/static/",http.StripPrefix("/static",fs))
-    http.ListenAndServe(":8080",nil)
-}
